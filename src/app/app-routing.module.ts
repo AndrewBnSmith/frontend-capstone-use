ead9490e59ad4ea27d51805829ee2933f68ba861
AndrewBnSmith <absmith1058@outlook.com>
2022-10-11 14:54:30 -0400

    updated home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,7 @@ const routes: Routes = [
   // {path:'register-success',redirectTo: 'home'},
   {path:'home',component:HomeComponent,
   children : [
+    { path : '' , component : MainComponent },
     { path : 'home' , component : MainComponent },
     { path : "profile" , component : ProfileComponent },
     { path : "create-goal" , component : CreateGoalComponent },
