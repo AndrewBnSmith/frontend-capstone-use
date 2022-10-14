d6a02a8b63c5ed2978d3b4f561e6df92e3361ad3
AzureAD\ChiSio <ivan202525@gmail.com>
2022-10-14 15:33:59 -0400

    welcome section

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GoalsService } from 'src/services/goals.service';
+import { RegistrationService } from 'src/services/registration.service';
 import { Goal } from '../Models/goal';
+import { User } from '../Models/user';
 
 @Component({
   selector: 'app-main',
@@ -11,14 +13,22 @@ import { Goal } from '../Models/goal';
 export class MainComponent implements OnInit {
   goals!: Goal[];
   goalList!: Goal[];
+  user_session : any;
+  user : User = new User();
 
 
-  constructor(private goalService : GoalsService , private router : Router) { }
+  constructor(private goalService : GoalsService , private router : Router, private service : RegistrationService) { }
 
   ngOnInit(): void {
     this.getGoals();
     this.getAllGoals();
+    this.user_session=sessionStorage.getItem("user_id");
+    this.service.GetUserById(this.user_session).subscribe(data => {
+      this.user_session = data;
+      console.log(this.user_session);
+  })
   }
+  
 
   getGoals(){
     this.goalService.getGoalsCount().subscribe(data => {
